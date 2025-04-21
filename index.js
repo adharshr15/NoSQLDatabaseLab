1160d5094adeb9f2966b868a84d0f27e7fbab298
adharshrajavel <adharshrajavel@tamu.edu>
2025-04-21 15:21:24 -0500

    fixed localhost

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
