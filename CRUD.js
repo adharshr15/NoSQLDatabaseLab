b06e364549a02d09451ae075b1dd27aac1151ed7
GEOSAR15 <101204558+GEOSAR15@users.noreply.github.com>
2025-04-21 15:41:09 -0500

    Create CRUD.js

---
+++ CRUD.js
@@ -0,0 +1,52 @@
+const createBallot = async (voterID, regPIN, firstChoice, secondChoice, thirdChoice) => {
+    const existing = await prisma.ballot.findFirst({
+      where: { voterID, regPIN },
+    });
+  
+    if (existing) {
+      throw new Error('This voter has already submitted a ballot.');
+    }
+  
+    const ballot = await prisma.ballot.create({
+      data: {
+        voterID,
+        regPIN,
+        firstChoice,
+        secondChoice,
+        thirdChoice,
+      },
+    });
+  
+    return ballot;
+  };
+  
+  
+const getBallots = async () => {
+    const ballots = await prisma.ballot.findMany();
+    return ballots;
+};
+  
+  
+const updateBallot = async (id, data) => {
+    const ballot = await prisma.vote.update({
+      where: { id },
+      data,
+    });
+    return ballot;
+};
+  
+  
+const deleteBallot = async (id) => {
+    await prisma.vote.delete({
+      where: { id },
+    });
+    return { message: 'Ballot deleted' };
+};
+  
+  
+module.exports = {
+    createBallot,
+    getBallots,
+    updateBallot,
+    deleteBallot
+  };
